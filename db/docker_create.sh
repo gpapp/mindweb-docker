a51811fd2c79d122afd48d5cd070a261c621fad5
Gergely Papp <gergely.papp@itworks.hu>
2015-06-02 11:04:20 +0200

    Fixed data intitialization

---
+++ db/docker_create.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+## This snippet is used by the build script to create container specific to the project
+
+docker create -P --name mw-db-1 mindweb/db
+
+echo "Starting db and trying to connect to it"
+docker start mw-db-1
+
+
+# Initialize db
+until docker run -it --link mw-db-1:cassandra --rm --volume `pwd`/scripts:/scripts cassandra sh -c 'exec cqlsh "$CASSANDRA_PORT_9042_TCP_ADDR" -f /scripts/init_sessions.cql'
+do 
+	echo "Retrying in 1 sec"
+	sleep 1
+done
+
+docker stop mw-db-1
