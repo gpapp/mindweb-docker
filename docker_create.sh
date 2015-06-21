cb8cea7bd30456086a7c7ad058fc8db822e2b773
gpapp <gergely.papp@itworks.hu>
2015-06-22 00:27:14 +0200

    NEW: Added  missing link

--- docker_create.sh
+++ docker_create.sh
@@ -2,5 +2,6 @@
 ## This snippet is used by the build script to create container specific to the project
 
 docker create -P --name mw-file-$TYPE \
+  --link mw-freeplane-converter-$TYPE:freeplane-converter \
   --link mw-storage-$TYPE:storage \
   mindweb/file
