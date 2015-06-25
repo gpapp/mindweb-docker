fb0b274171954ae438a2729270153e571adbec55
Gergely Papp <gergely.papp@itworks.hu>
2015-06-25 09:46:24 +0200

    Added possibility to query other environments as well

--- show_ports.sh
+++ show_ports.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-
-echo "DB:$(docker port mw-db-DEV |grep 9042)"
-echo "SESSION:$(docker port mw-session-manager-DEV | grep 2000)"
-echo "STORAGE:$(docker port mw-storage-DEV | grep 2001)"
-echo "CONVERT:$(docker port mw-freeplane-converter-DEV | grep 2002)"
-echo "FILE:$(docker port mw-file-DEV | grep 2003)"
-echo "BROKER:$(docker port mw-broker-DEV | grep 8080)"
-echo "UI:$(docker port mw-ui-DEV |grep 8080)"
+TYPE=$1
+if [ -n $TYPE ]; then
+  TYPE='DEV'
+fi
+echo "DB:$(docker port mw-db-${TYPE} |grep 9042)"
+echo "SESSION:$(docker port mw-session-manager-${TYPE} | grep 2000)"
+echo "STORAGE:$(docker port mw-storage-${TYPE} | grep 2001)"
+echo "CONVERT:$(docker port mw-freeplane-converter-${TYPE} | grep 2002)"
+echo "FILE:$(docker port mw-file-${TYPE} | grep 2003)"
+echo "BROKER:$(docker port mw-broker-${TYPE} | grep 8080)"
+echo "UI:$(docker port mw-ui-${TYPE} |grep 80)"
